2748bd851a2cd1c297397ea5db2db49b1eff9f00
dskoglund <dskoggz@gmail.com>
2016-08-30 10:41:08 -0700

    Add files via upload

---
+++ lastattempt.js
@@ -0,0 +1,201 @@
+var data = `[{
+  "name": "Robert Palmer",
+  "image": "https://robohash.org/eiussolutadolorem.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Katherine Oliver",
+  "image": "https://robohash.org/numquaminventoremagnam.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Kenneth Simmons",
+  "image": "https://robohash.org/delectusuteum.jpg?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Phyllis Fields",
+  "image": "https://robohash.org/facereautminima.bmp?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Steven Chavez",
+  "image": "https://robohash.org/facilisodiomagni.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Ruth Carr",
+  "image": "https://robohash.org/illumoptiosoluta.bmp?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Mark Alexander",
+  "image": "https://robohash.org/aeumsit.bmp?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Ralph Hicks",
+  "image": "https://robohash.org/quianesciuntquos.bmp?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Douglas Johnston",
+  "image": "https://robohash.org/nostrumquioccaecati.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Ryan Anderson",
+  "image": "https://robohash.org/quamquianemo.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Julie Castillo",
+  "image": "https://robohash.org/nonsitrerum.bmp?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Deborah Owens",
+  "image": "https://robohash.org/inmodiad.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Donna Howell",
+  "image": "https://robohash.org/quasisedaperiam.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Alan Lane",
+  "image": "https://robohash.org/quoeaquequod.bmp?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Johnny Grant",
+  "image": "https://robohash.org/itaquequosunt.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Jose Jordan",
+  "image": "https://robohash.org/deseruntnoncum.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Victor King",
+  "image": "https://robohash.org/velitdebitisad.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Lisa Fox",
+  "image": "https://robohash.org/explicaboofficiavel.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Patrick Mason",
+  "image": "https://robohash.org/vitaeestaut.jpg?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Joe Nichols",
+  "image": "https://robohash.org/velitsuntest.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Katherine Sullivan",
+  "image": "https://robohash.org/eossaepecupiditate.jpg?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Kathy Morris",
+  "image": "https://robohash.org/officiaperspiciatisnatus.jpg?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Linda Elliott",
+  "image": "https://robohash.org/natusculpacorporis.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Steven Kelly",
+  "image": "https://robohash.org/quidemvelquas.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Gloria Mason",
+  "image": "https://robohash.org/utrepellendusiusto.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Joe Hanson",
+  "image": "https://robohash.org/quibusdamautemneque.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Paul Brown",
+  "image": "https://robohash.org/quosquialaboriosam.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Jack Bishop",
+  "image": "https://robohash.org/solutaconsequatura.bmp?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Harold Adams",
+  "image": "https://robohash.org/recusandaeeosdolorum.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Karen Chavez",
+  "image": "https://robohash.org/quiearumaperiam.bmp?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Barbara King",
+  "image": "https://robohash.org/dolorumetaut.jpg?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Harold Bowman",
+  "image": "https://robohash.org/rerumerrorlabore.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Martin Welch",
+  "image": "https://robohash.org/etvoluptasnesciunt.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Justin Burke",
+  "image": "https://robohash.org/quivelin.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Theresa Hawkins",
+  "image": "https://robohash.org/doloresfaceremagni.bmp?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Juan Rogers",
+  "image": "https://robohash.org/harumlaudantiumid.bmp?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Angela Chapman",
+  "image": "https://robohash.org/etomnisrerum.bmp?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Margaret Butler",
+  "image": "https://robohash.org/avoluptatummolestias.jpg?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Anthony Hart",
+  "image": "https://robohash.org/nonmagnamquo.jpg?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Todd Carroll",
+  "image": "https://robohash.org/architectorecusandaecumque.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Melissa Kennedy",
+  "image": "https://robohash.org/laudantiumseddolorem.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Randy Woods",
+  "image": "https://robohash.org/porroinciduntet.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Carlos Olson",
+  "image": "https://robohash.org/sedvoluptatescumque.bmp?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Ryan Ruiz",
+  "image": "https://robohash.org/enimcumqueomnis.png?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Roy Stevens",
+  "image": "https://robohash.org/repudiandaeabad.jpg?size=50x50&set=set1",
+  "friend": true
+}, {
+  "name": "Stephen Wagner",
+  "image": "https://robohash.org/suntdoloremquein.bmp?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Anna Arnold",
+  "image": "https://robohash.org/sedlaboreeos.bmp?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Lois Owens",
+  "image": "https://robohash.org/quiquiaquaerat.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Thomas Perkins",
+  "image": "https://robohash.org/eiusmolestiaererum.png?size=50x50&set=set1",
+  "friend": false
+}, {
+  "name": "Patrick Coleman",
+  "image": "https://robohash.org/nequecommodiaut.png?size=50x50&set=set1",
+  "friend": true
+}]`
